7a529c195e5e314d6b7554cf106d2758222739be
Brian Bailey <brianbailey@designmentor.com>
2015-09-23 13:00:15 -0400

    Changed to analog sensors and added debug output

--- code/sumobot/sumobot.ino
+++ code/sumobot/sumobot.ino
@@ -16,14 +16,14 @@
 /*******************************************************
 * Define statements - Define constants we will use later
 *******************************************************/
-#define TRIGGER_PIN  A0      // Arduino pin tied to trigger pin on the ultrasonic sensor.
-#define ECHO_PIN     A1      // Arduino pin tied to echo pin on the ultrasonic sensor.
-#define LEFT_WHEEL_PIN  A2   // Arduino pin tied to the left servo wheel motor
-#define RIGHT_WHEEL_PIN A3   // Arduino pin tied to the right servo wheel motor
-#define MAX_DISTANCE 200     // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
-#define FRONT_EDGE_SENSOR 2  // Pin connected to the front edge IR sensor
-#define REAR_EDGE_SENSOR 3   // Pin connected to the rear edge IR sensor
-#define LIGHT_COLOR_VALUE 2900 // This is a value returned from the edge sensors when they see a lighter color, may need tweaking
+#define TRIGGER_PIN       A0    // Arduino pin tied to trigger pin on the ultrasonic sensor.
+#define ECHO_PIN          A1    // Arduino pin tied to echo pin on the ultrasonic sensor.
+#define LEFT_WHEEL_PIN    A2    // Arduino pin tied to the left servo wheel motor
+#define RIGHT_WHEEL_PIN   A3    // Arduino pin tied to the right servo wheel motor
+#define MAX_DISTANCE      200   // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
+#define FRONT_EDGE_SENSOR A4    // Pin connected to the front edge IR sensor
+#define REAR_EDGE_SENSOR  A5    // Pin connected to the rear edge IR sensor
+#define LIGHT_COLOR_VALUE 512   // This is a value returned from the edge sensors when they see a lighter color, may need tweaking
 
 /********************************************************
 * Class object instances - These will help us control the
@@ -67,28 +67,30 @@ void loop()
 	* IR Edge finder code
 	********************************/
 	// Check front and back edge sensors for edge of the ring
-	// Lower numbers mean more reflective colors
-	// 3000 or more means nothing was reflected.
-	frontEdgeValue = readQD(FRONT_EDGE_SENSOR);
+	// Lower numbers mean less reflective colors
+	// 1023 means nothing was reflected.
+	frontEdgeValue = analogRead(FRONT_EDGE_SENSOR);
 	// Print front edge value for debug
 	Serial.print("Front Edge: ");
 	Serial.println(frontEdgeValue);
-	// Less than 1000 means we see white-ish
+	// Less than LIGHT_COLOR_VALUE means we see a dark color
 	// This number can be tweaked if the IR sensor is closer to the ground
 	if(frontEdgeValue > LIGHT_COLOR_VALUE)
 	{
 		// Better back up!!
+		Serial.println("Front edge detected, backing up!");
 		leftWheel.write(180);
 		rightWheel.write(0);
 		delay(1000);
 	}
 	// Now check the back edge
-	int backEdgeValue = readQD(REAR_EDGE_SENSOR);
+	int backEdgeValue = analogRead(REAR_EDGE_SENSOR);
 	Serial.print("Back Edge: ");
 	Serial.println(backEdgeValue);
 	if(backEdgeValue > LIGHT_COLOR_VALUE)
 	{
 		// Better go forward!!
+		Serial.println("Back edge detected, moving forward!");
 		leftWheel.write(0);
 		rightWheel.write(180);
 		delay(1000);
@@ -107,34 +109,16 @@ void loop()
 
 	// If don't see opponent, spin to look around
 	if(distInCentimeters > 39 || distInCentimeters == 0){
+		Serial.println("Spinning");
 		leftWheel.write(180);
 		rightWheel.write(180);
 	}
 	// Otherwise attack!
 	else {
+		Serial.println("Attacking");
 		leftWheel.write(0);
 		rightWheel.write(180);
 	}
 	
 	delay(50);
-}
-
-
-
-int readQD(int pin){
-	//Returns value from the QRE1113
-	//Lower numbers mean more refleacive
-	//More than 3000 means nothing was reflected.
-	pinMode( pin, OUTPUT );
-	digitalWrite( pin, HIGH );
-	delayMicroseconds(10);
-	pinMode( pin, INPUT );
-
-	long time = micros();
-
-	//time how long the input is HIGH, but quit after 3ms as nothing happens after that
-	while (digitalRead(pin) == HIGH && micros() - time < 3000);
-	int diff = micros() - time;
-
-	return diff;
 }
